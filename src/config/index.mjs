ba3d77e2a7b3d954ce876d53f97da4626d699058
trumanpurnell <truman.purnell@gmail.com>
2019-07-08 21:07:00 -0700

    strips myio to bare bones essentials

--- src/config/index.mjs
+++
@@ -1,3 +0,0 @@
-export default {
-    FRAME_RATE: 30
-}
--- src/index.mjs
+++ src/index.mjs
@@ -1,22 +1,9 @@
-import MyIOSocket from './sources/socket'
-import MyIODragDrop from './sources/dragdrop'
+import bb from 'big-box'
 
-class MyIO extends Array {
-    constructor() {
-        super()
-
-        this.socket = new MyIOSocket()
-        this.dragdrop = new MyIODragDrop()
-    }
-
-    async load(path) {
+export default class MyIO {
+    async static imread(path) {
         const response = await fetch(path)
-
-        return response.blob()
-    }
-
-    async imread(path) {
-        const imageBlob = await this.load(path)
+        const imageBlob = await response.blob()
         const bitmap = await createImageBitmap(imageBlob)
 
         const canvas = document.createElement('canvas')
@@ -33,13 +20,8 @@ class MyIO extends Array {
             .array({ with: data })
             .reshape({ shape: [bitmap.width, bitmap.height, 4] })
 
-        this.push(image)
-
         canvas.remove()
 
         return image
     }
-
 }
-
-export default new MyIO()
