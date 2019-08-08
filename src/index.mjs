ec88a74a448fe5dada929c30eaf4629386b08fc2
trumanpurnell <truman.purnell@gmail.com>
2019-08-07 23:37:19 -0700

    removes depenendcies eg. bigbox, radley

--- src/index.mjs
+++ src/index.mjs
@@ -1,27 +1,28 @@
-import bb from 'big-box'
 
 export default class MyIO {
     static async imread(path) {
+        // Build canvas init context
+        const canvas = document.createElement('canvas')
+        const context = canvas.getContext('2d')
+
+        // fetch
         const response = await fetch(path)
         const imageBlob = await response.blob()
         const bitmap = await createImageBitmap(imageBlob)
 
-        const canvas = document.createElement('canvas')
-        const context = canvas.getContext('2d')
+        // paint
         const region = [0, 0, bitmap.width, bitmap.height]
-
         canvas.width = bitmap.width
         canvas.height = bitmap.height
-
         context.drawImage(bitmap, ...region)
 
-        const data = context.getImageData(...region).data
-        const image = bb
-            .array({ with: data })
-            .reshape({ shape: [bitmap.width, bitmap.height, 4] })
-
+        // clean up
         canvas.remove()
 
-        return { image, bitmap }
+        return {
+            shape: [bitmap.width, bitmap.height, 4],
+            pixels: context.getImageData(...region).data,
+            binary: bitmap
+        }
     }
 }
